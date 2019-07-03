b3e68763ef5c37289d6791b2560ac4c4f185974b
ElianNasatto <elian.nasa123@gmail.com>
2019-07-03 07:53:53 -0300

    Criação do Projeto e Index

---
+++ ExemploWeb01/View/Controllers/ColaboradorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class ColaboradorController : Controller
+    {
+        // GET: Colaborador
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cadastro(string nome, string sobrenome)
+        {
+            return View();
+        }
+    }
+}
